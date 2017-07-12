2dcacae229934464a34377ab90066ddd22e63fd3
meenacseengineering5 <meenacse1997@gmail.com>
2017-07-12 08:48:35 -0700

    Add files via upload

---
+++ snake.java
@@ -0,0 +1,29 @@
+package encapsulation;
+import java.util.*;
+public class snake {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		  int n=s.nextInt();
+		  int i,j;
+		  for(i=0;i<n;i++)
+		  {
+		   if(i==0||i==n-1)
+		    for(int k=0;k<n;k++)
+		      System.out.print("* ");
+		   else
+		   {
+		    for(j=0;j<n;j++)
+		    {
+		      if(j==0||j==n-1)
+		       System.out.print("* ");
+		      else
+		       System.out.print("  ");
+		    }
+		    
+		   }
+		   System.out.println();
+		  }
+		 }
+
+}
